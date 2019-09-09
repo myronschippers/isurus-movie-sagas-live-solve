1107a56c7539d436915938f4b04f7fdbecf347dd
Myron <dubtwee@gmail.com>
2019-09-09 10:00:01 -0500

    adding an API endpoint for updating a movie

--- server/routers/movie.router.js
+++ server/routers/movie.router.js
@@ -37,4 +37,21 @@ router.get('/single/:id', (req, res) => {
         })
 });
 
+// Update movie Title and Description for a single movie
+router.put('/single/:id', (req, res) => {
+    const newMovieData = req.body;
+    const movieId = req.params.id;
+    const queryText = `UPDATE "movies" SET "title" = $1, "description" = $2
+                        WHERE "id" = $3;`;
+
+    pool.query(queryText, [newMovieData.title, newMovieData.description, movieId])
+        .then((response) => {
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log('Error with PUT (single): ', err);
+            res.sendStatus(500);
+        });
+});
+
 module.exports = router;
