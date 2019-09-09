c8ee6a9a0feddaa01dd4a377a489498714db984b
Myron <dubtwee@gmail.com>
2019-09-09 09:48:36 -0500

    adding a get API endpoint for retrieving a single movie

--- server/routers/movie.router.js
+++ server/routers/movie.router.js
@@ -20,8 +20,21 @@ router.get('/', (req, res) => {
 
 // Get a single movie
 // join with Genres
-router.get('/single', (res, req) => {
-    
+router.get('/single/:id', (req, res) => {
+    const queryText = `SELECT * FROM "movies"
+                        JOIN "movies_genres" ON "movies".id = "movies_genres".movie_id
+                        JOIN "genres" ON "movies_genres".genre_id = "genres".id
+                        WHERE "movies".id = $1;`
+    const movieId = req.params.id;
+
+    pool.query(queryText, [movieId])
+        .then((response) => {
+            res.send(response.rows);
+        })
+        .catch((err) => {
+            console.log('Error with GET (single): ', err);
+            res.sendStatus(500);
+        })
 });
 
 module.exports = router;
