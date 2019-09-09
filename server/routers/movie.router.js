320b2d9969384c171c07c86074eff46a4dae721c
Myron <dubtwee@gmail.com>
2019-09-09 09:33:59 -0500

    adding get all movies API endpoint

--- server/routers/movie.router.js
+++ server/routers/movie.router.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool');
 
 // Getting entire list of movies
 // does not need genres
-router.get('/', (res, req) => {
+router.get('/', (req, res) => {
+    const queryText = `SELECT * FROM "movies"
+                        ORDER BY "title" ASC;`
 
+    pool.query(queryText)
+        .then((response) => {
+            res.send(response.rows);
+        })
+        .catch((err) => {
+            console.log('Error with GET: ', err);
+            res.sendStatus(500);
+        });
 });
 
 // Get a single movie
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const movieRouter = require('./routers/movie.router');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
